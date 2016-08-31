b77953a93badadc76d4cab31901621e554222372
Leandro Pineda <leandropineda.lp@gmail.com>
2016-08-30 23:16:50 -0300

    Exercise 2 completed.

--- cifasis/imgfun.py
+++ cifasis/imgfun.py
@@ -1,5 +1,6 @@
 import random as rnd
-
+import numpy as np
+from PIL import Image
 
 def extract_window(img, window_size=None):
     """
@@ -12,7 +13,7 @@ def extract_window(img, window_size=None):
         window_size = [16, 16]
 
     img_size = img.size
-
+    
     # if the image is smaller than window_size, a subimage cannot be extracted
     if img_size[0] < window_size[0] or img_size[1] < window_size[1]:
         raise Exception("Can't extract subimage")
@@ -21,5 +22,38 @@ def extract_window(img, window_size=None):
     rnd_x = int(rnd.random()*(img_size[0]-window_size[0]))
     rnd_y = int(rnd.random()*(img_size[1]-window_size[1]))
 
-    return img.crop([rnd_x, rnd_x + img_size[0],
-                     rnd_y, rnd_y + img_size[1]])
+    return img.crop((rnd_x, rnd_y, rnd_x + window_size[0], rnd_y + window_size[1]))
+    
+def normalize(np_img):
+    """
+    Returns np_img with mean equal to zero and std dev equal to one
+    :param np_img: numpy matrix
+    """
+
+    mean = np.mean(np_img)
+    std = np.std(np_img)
+    
+    np_img = (np_img - mean) / std
+    
+    return np_img
+
+def quantize(np_img):
+    """
+    Returns a quantized version of np_img in the range [0, 255].
+    :param np img: numpy matrix
+    """
+    
+    min_img = np.min(np_img)
+    max_img = np.max(np_img)
+    
+    range_img = max_img - min_img
+    
+    f = np.vectorize(lambda x: np.int8(255*(x-min_img)/range_img))
+    
+    quantized_img = f(np_img)
+    img =  Image.fromarray(quantized_img,"L")
+
+    return img
+    
+    
+    
--- tp1/main.py
+++ tp1/main.py
@@ -9,9 +9,20 @@ def main():
     width = 450
     height = 450
     
-    path_images = parse_dataset_dir('/home/lpineda/101_ObjectCategories/watch')
+    path_images = parse_dataset_dir('/home/lpineda/101_ObjectCategories/')
+    
 
-    dataset = read_dataset(path_images, width, height)
+    #dataset = read_dataset(path_images, width, height)
+    
+    test_img =  read_image(path_images[3958])
+    test_img.show()
+    
+    cropped_img = extract_window(test_img)
+    
+    rr, gg, bb = test_img.split()
+    
+    quan_rr = quantize(normalize(np.array(bb)))
+    quan_rr.show()
     
 
 if __name__ == "__main__":
