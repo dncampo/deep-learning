8f2d1bcfa54aa151d7d871efde1b85d8015e42a5
Leandro Bugnon <leandro@adahome.com>
2016-09-24 01:05:26 -0300

    merge

--- tp2/keras_mnist.py
+++ tp2/keras_mnist.py
@@ -38,14 +38,14 @@ Y_test = np_utils.to_categorical(y_test, nb_classes)
 model = Sequential()
 
 # Original =======================================
-model.add(Dense(512, input_shape=(784,)))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(Dense(512))
-model.add(Activation('relu'))
-model.add(Dropout(0.2))
-model.add(Dense(10))
-model.add(Activation('softmax'))
+# model.add(Dense(512, input_shape=(784,)))
+# model.add(Activation('relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(512))
+# model.add(Activation('relu'))
+# model.add(Dropout(0.2))
+# model.add(Dense(10))
+# model.add(Activation('softmax'))
 # ================================================
 # mod ii =========================================
 # model.add(Dense(512, input_shape=(784,)))
@@ -74,12 +74,12 @@ model.add(Activation('softmax'))
 # model.add(Activation('softmax'))
 # ================================================
 # mod v ==========================================
-# model.add(Dense(512, input_shape=(784,)))
-# model.add(Activation('relu'))
-# model.add(Dense(512))
-# model.add(Activation('relu'))
-# model.add(Dense(10))
-# model.add(Activation('softmax'))
+model.add(Dense(512, input_shape=(784,)))
+model.add(Activation('relu'))
+model.add(Dense(512))
+model.add(Activation('relu'))
+model.add(Dense(10))
+model.add(Activation('softmax'))
 # ================================================
 model.summary()
 
