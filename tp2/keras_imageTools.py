5458af77d705af237957afd08da6537836488c81
Leandro Bugnon <leandro@adasinc.com>
2016-09-29 17:56:12 -0300

    update

--- tp2/keras_imageTools.py
+++ tp2/keras_imageTools.py
@@ -5,7 +5,8 @@ from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.optimizers import SGD, Adam, RMSprop
 from keras.preprocessing.image import ImageDataGenerator
-from keras.layers.core import Reshape
+from keras.layers.core import Reshape,Flatten
+from keras.layers.convolutional import Convolution2D,MaxPooling2D
 #plt.close('all')
 
 # airplanes vs motorbikes
@@ -14,53 +15,83 @@ validDir='/home/leandro/workspace/Dropbox/ref/deeplearning_cifasis/data/airplane
 
 # cargador por defecto. Cada subfolder es una clase.
 #imageGen=ImageDataGenerator(featurewise_std_normalization=True,  featurewise_center=True)
-imageGen=ImageDataGenerator()
+imageGenTrain=ImageDataGenerator(rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+                                 fill_mode='nearest')
+imageGenTest=ImageDataGenerator(rescale=1./255)
 
-train_generator=imageGen.flow_from_directory(trainDir) 
-
-valid_generator=imageGen.flow_from_directory(validDir) 
-
-# TODO: como hacer normalización desde el flow_directory??
-
-# batch 32 y 128: Test score: 8.05904769897, Test accuracy: 0.5
-
-
-batch_size = 32
+imsize=128
+batch_size = 128
 nb_classes = 2
-nb_epoch = 5
+nb_epoch = 20
+
+train_generator=imageGenTrain.flow_from_directory(trainDir,target_size=(imsize, imsize),batch_size=batch_size, class_mode='binary')
+#,save_to_dir='/home/leandro/tmp/')
+
+valid_generator=imageGenTest.flow_from_directory(validDir,target_size=(imsize, imsize),batch_size=batch_size, class_mode='binary')
+
+# train_generator=imageGenTrain.flow_from_directory(trainDir,target_size=(imsize, imsize)) # categorical labels
+
+
+
 
 trainSamples=train_generator.N
 validationSamples=valid_generator.N
 
 model = Sequential()
 
-#model.add(Dense(512, input_shape=(784,)))
-# O hago un reshape del input, o uso otro tipo de capa...
-# model.add(Dense(512, input_shape=(256,256,3,)))
-model.add(Reshape((256*256*3,), input_shape=(256,256,3)))
-model.add(Dense(100, input_shape=(256*256*3,)))
+
+# esto creo que no anda
+#model.add(Reshape((imsize*imsize*3,), input_shape=(imsize,imsize,3)))
+
+model.add(Convolution2D(32, 3, 3, input_shape=(imsize, imsize,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Convolution2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten()) 
+#model.add(Flatten(input_shape=(imsize,imsize,3))) 
+
+model.add(Dense(100))
 model.add(Activation('relu'))
 # model.add(Dropout(0.2))
 # model.add(Dense(512))
 # model.add(Activation('relu'))
 # model.add(Dropout(0.2))
-model.add(Dense(nb_classes)) 
-model.add(Activation('softmax'))
+# model.add(Dense(2))
+# model.add(Activation('softmax'))
+
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+
 
 model.summary()
 
-model.compile(loss='categorical_crossentropy',
+model.compile(loss='binary_crossentropy',
               optimizer=RMSprop(),
               metrics=['accuracy'])
 
 history = model.fit_generator(train_generator,
-                    samples_per_epoch=batch_size, nb_epoch=nb_epoch,
+                    samples_per_epoch=batch_size*20, nb_epoch=nb_epoch,
                               verbose=1)
 # history = model.fit_generator(train_generator,
 #                     samples_per_epoch=batch_size, nb_epoch=nb_epoch,
-#                               verbose=1, validation_data=train_generator)
+#                               verbose=1, validation_data=train_generator,nb_val_samples=batch_size)
 score = model.evaluate_generator(valid_generator,validationSamples, verbose=1)
 
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
-model.save('keras_image.h5')
+#model.save('keras_image.h5')
